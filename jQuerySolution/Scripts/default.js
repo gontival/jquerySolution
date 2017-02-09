91bd7429cb75b8d94f58bc522127c4a1e5eb33db
MCNEXT\jlevant <jlevant@mcnext.com>
2017-02-09 16:37:29 +0100

    using jQuery

---
+++ jQuerySolution/Scripts/default.js
@@ -0,0 +1,14 @@
+﻿var txtInput;
+var txtResult;
+
+function initialize() {
+    txtInput = document.getElementById('txtInput');
+    txtResult = document.getElementById('txtResult');
+    clear();
+}
+
+function clear() {
+    txtInput.value = '0';
+    txtResult.value = '0';
+}
+
